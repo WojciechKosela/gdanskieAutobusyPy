ac9059dec9f66839b5f0d32890b910e430dcdaaa
wojtek <wojtek.kosela@wp.pl>
2023-10-14 09:00:54 +0200

    init

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from download import download_json
 
 if __name__ == '__main__':
-    ## download_json("https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/4c4025f0-01bf-41f7-a39f-d156d201b82b/download/stops.json","stops.json")
+    # download_json("https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/4c4025f0-01bf-41f7-a39f-d156d201b82b/download/stops.json","stops.json")
 
     stops = []
     with open("stops.json", "r") as read_file:
